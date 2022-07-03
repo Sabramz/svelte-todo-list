7a550ecd884f8d2524e33ee9cfbe3929088dc724
Scott A <scottrabramson@gmail.com>
2022-07-03 08:04:52 -0400

    /docs dir created for gh pages

---
+++ docs/entries/pages/index.svelte.js
@@ -0,0 +1,9 @@
+import { c as create_ssr_component } from "../../chunks/index-2ba0e0c3.js";
+const Routes = create_ssr_component(($$result, $$props, $$bindings, slots) => {
+  return `${$$result.head += `${$$result.title = `<title>Scott&#39;s Portfolio</title>`, ""}`, ""}
+
+<h1>Welcome</h1>
+
+<a href="${"./TodoList/TodoList"}">Svelte TodoList</a>`;
+});
+export { Routes as default };
---
+++ docs/nodes/2.js
@@ -0,0 +1,7 @@
+import * as module from '../entries/pages/TodoList/TodoList.svelte.js';
+
+export { module };
+export const index = 2;
+export const entry = 'pages/TodoList/TodoList.svelte-7bf337bf.js';
+export const js = ["pages/TodoList/TodoList.svelte-7bf337bf.js","chunks/index-331b8c1c.js"];
+export const css = ["assets/pages/TodoList/TodoList.svelte-74997251.css"];
